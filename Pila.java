f9247b57174063605c447e7eb539fc8f77b2f7e6
Bamo0507 <bryanalbertomo@gmail.com>
2024-01-28 20:15:06 -0600

    Se completaron 2 métodos de la interfaz

--- Pila.java
+++ Pila.java
@@ -1,7 +1,24 @@
 import java.util.Vector;
 
-public class Pila implements InterfazUVGStack<String>{
-    private Vector<String> numeros; 
-    
+public class Pila implements InterfazUVGStack<Integer>{
+    private Vector<Integer> numeros; 
+
+    public Pila(){
+        this.numeros = new Vector();
+    }
+
+    //Verificar si está llenó o vacío
+    public boolean isEmpty(){
+        if(numeros == null){
+            return true; //Devuelve verdadero si sí está vacío
+        } else {
+            return false; //Devuelve falso si no está vacío
+        }
+    }
+
+    //Método para meter valores al Vector
+    public void push(Integer numero){
+        numeros.add(numero);
+    }
 
 }
